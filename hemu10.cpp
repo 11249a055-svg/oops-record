3437d508416c3481e1fc1dcff6bff5dac75b68d6
11249a055-svg <11249a055@kanchiuniv.ac.in>
2025-11-07 03:07:48 -0800

    Add files via upload

---
+++ hemu10.cpp
@@ -0,0 +1,25 @@
+  #include<iostream>
+  using namespace std;
+  class match
+  {
+    public:
+    int add(int a,intb)
+    {
+        return a+b;
+    }
+    double add(double a,double b)
+}
+        int add(int a,int b,int c)
+        {
+        double add(double a,double b,double c)
+            return a+b+c;
+        }
+    };
+    int main()
+    {
+        match m;
+        cout<<"sum of 2 int:"<<m.add(5,10)<<endl;
+        cout<<"sum of 2 double:"<<m.add(2.5,3.7)<<endl;
+        cout<<"sum of 3 int:"<<m.add(1,2,3)<<endl;
+        return 0;
+    }
